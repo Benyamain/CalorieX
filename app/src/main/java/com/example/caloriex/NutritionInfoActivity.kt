b30ea967dff4b5f32d7c196327fbffe9c94ae956
Benyamain <yacoobbenyamain@gmail.com>
2023-03-09 20:43:18 -0500

    Implemented the UI that will be used when scanning the food with the camera using the machine learning model where users can then add the food to that current day or cancel it. For some reason, I am not able to make NutritionInfoActivity.kt a popup so that needs fixing. For the most part, all the UI components are there and can now start to move toward programming implementation to handle these UI interactions.

---
+++ app/src/main/java/com/example/caloriex/NutritionInfoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.caloriex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.Window
+
+class NutritionInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nutrition_info)
+    }
+}
