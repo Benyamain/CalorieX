51e425307f467c828eaf46d2ac8e16e1a60664ac
Benyamain <yacoobbenyamain@gmail.com>
2023-03-14 04:30:56 -0400

    Started to implement the Charts aspect of this app. Not done but it is a good start! Will consider revising logic of the CalendarView to hopefully make it work in the next commit.

--- app/src/main/java/com/example/caloriex/DashboardFragment.kt
+++ app/src/main/java/com/example/caloriex/DashboardFragment.kt
@@ -1,5 +1,7 @@
 package com.example.caloriex
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,12 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
@@ -17,6 +25,11 @@ class DashboardFragment : Fragment() {
 
     private lateinit var navigationView: BottomNavigationView
     private lateinit var navController: NavController
+    private lateinit var caloriePieChart: PieChart
+    private lateinit var proteinPieChart: PieChart
+    private lateinit var carbsPieChart: PieChart
+    private lateinit var fatPieChart: PieChart
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +40,11 @@ class DashboardFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
         navigationView = view.findViewById(R.id.bottom_navigation)
+        caloriePieChart = view.findViewById(R.id.pieChartCaloricBudget)
+        proteinPieChart = view.findViewById(R.id.pieChartProtein)
+        carbsPieChart = view.findViewById(R.id.pieChartCarbs)
+        fatPieChart = view.findViewById(R.id.pieChartFat)
+
         navController = findNavController()
         navigationView.setupWithNavController(navController)
 
@@ -60,6 +78,20 @@ class DashboardFragment : Fragment() {
             }
         }
 
+        val entries: ArrayList<PieEntry> = ArrayList()
+        entries.add(PieEntry(70f))
+
+        val dataSet = PieDataSet(entries, "Mobile OS")
+        dataSet.setDrawIcons(false)
+        dataSet.color = Color.rgb(135,182,120)
+
+        val data = PieData(dataSet)
+
+        setupPieChart(caloriePieChart, data)
+        setupPieChart(proteinPieChart, data)
+        setupPieChart(carbsPieChart, data)
+        setupPieChart(fatPieChart, data)
+
         return view
     }
 
@@ -76,4 +108,22 @@ class DashboardFragment : Fragment() {
         // Set the selected item of bottom navigation view when the fragment is resumed
         //navigationView.menu.findItem(navController.currentDestination.id).isChecked = true
     }
+
+    private fun setupPieChart(pieChart: PieChart, data: PieData) {
+        pieChart.setUsePercentValues(true)
+        pieChart.description.isEnabled = false
+        pieChart.dragDecelerationFrictionCoef = 0.95f
+        pieChart.setDrawCenterText(true)
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setHoleColor(Color.rgb(45,42,62))
+        pieChart.rotationAngle = 0f
+        pieChart.isRotationEnabled = true
+        pieChart.isHighlightPerTapEnabled = true
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+        pieChart.legend.isEnabled = false
+        pieChart.data = data
+        pieChart.highlightValues(null)
+        pieChart.invalidate()
+    }
+
 }
